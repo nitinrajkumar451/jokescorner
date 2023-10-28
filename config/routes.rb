081c978c569690723c0ad968fa1cd5eefe62ef1b
nitin <nitin@incubyte.co>
2023-10-28 23:05:41 +0530

    ADD: Homepage controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :jokes
-  root 'jokes#index' 
-
+  root 'home_page#index' 
+  # get "/homepage/", to: "homepage#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
